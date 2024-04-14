e92a368102b0a3ef772c9632f5c6ac1780f1f73e
[fuad Ayo] <fuadayodeji@gmail.com>
2024-04-14 10:26:56 +0100

    addition of default hello world message for server

--- main.tf
+++ main.tf
@@ -4,6 +4,12 @@ provider "aws" {
 resource "aws_instance" "my_instance" {
   ami = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+user_data = <<-EOF
+#!/bin/bash
+echo "Hello, World" > index.xhtml
+nohup busybox httpd -f -p 8080 &
+EOF
+user_data_replace_on_change = true
   
   tags = {
     Name = "terraform-example"
